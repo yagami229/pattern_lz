1fed4a5a3349078e02417251b876a9a383b6ab5a
yagami229 <ilushkkins1226@tutamail.com>
2026-06-08 20:37:49 +0300

    strategy.py

--- strategy.py
+++ strategy.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 class NotificationSender(ABC):
     @abstractmethod
     async def send_message(self, text: str) -> str:
-        """Каждая стратегия должна реализовать этот метод асинхронно"""
         pass
 
 class EmailDelivery(NotificationSender):
